9a906da752d0595daa00c5ddf44efbe1aff5b2ed
JASONXU <xujason419@outlook.com>
2024-04-25 22:39:28 -0300

    feat: interaction polish

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,20 @@
-const image = document.getElementsByClassName('parallax');
-new simpleParallax(image,{
-    scale: 1.2,
-    transition: 'cubic-bezier(0,0,0,1)'
+// const image = document.getElementsByClassName('parallax');
+// new simpleParallax(image,{
+//     scale: 1.2,
+//     transition: 'cubic-bezier(0,0,0,1)',
+//     maxTransition: 0.6,
+// });
+
+// add a class to nav when scrolled to 100vh
+
+const nav = document.querySelector('#main-nav');
+
+document.addEventListener('scroll', () => {
+    if(window.scrollY >= document.documentElement.clientHeight) {
+        nav.classList.add('shrink');
+        nav.classList.remove('grow');
+    } else {
+        nav.classList.remove('shrink');
+        nav.classList.add('grow');
+    }
 });
